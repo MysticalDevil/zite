eee9d34ec29056bff8a0223186a39a9f763850f8
MysticalDevil <jinpeng7981@outlook.com>
2026-02-24 19:25:34 +0800

    refactor: seal raw API to db/stmt only

--- src/raw/mod.zig
+++ src/raw/mod.zig
@@ -1,8 +1,5 @@
 const sqlite = @import("sqlite3.zig");
 
-pub const sqlite3 = sqlite.sqlite3;
-pub const sqlite3_stmt = sqlite.sqlite3_stmt;
-
 pub const Rc = i32;
 
 pub const SQLITE_OK: Rc = @intCast(sqlite.SQLITE_OK);
@@ -22,32 +19,6 @@ pub const SQLITE_NOMEM: Rc = @intCast(sqlite.SQLITE_NOMEM);
 
 pub const DbHandle = sqlite.DbHandle;
 pub const StmtHandle = sqlite.StmtHandle;
-
-pub const sqlite3_open = sqlite.sqlite3_open;
-pub const sqlite3_close = sqlite.sqlite3_close;
-pub const sqlite3_close_v2 = sqlite.sqlite3_close_v2;
-pub const sqlite3_errmsg = sqlite.sqlite3_errmsg;
-pub const sqlite3_free = sqlite.sqlite3_free;
-pub const sqlite3_exec = sqlite.sqlite3_exec;
-pub const sqlite3_prepare_v2 = sqlite.sqlite3_prepare_v2;
-pub const sqlite3_finalize = sqlite.sqlite3_finalize;
-pub const sqlite3_step = sqlite.sqlite3_step;
-pub const sqlite3_reset = sqlite.sqlite3_reset;
-pub const sqlite3_clear_bindings = sqlite.sqlite3_clear_bindings;
-pub const sqlite3_bind_null = sqlite.sqlite3_bind_null;
-pub const sqlite3_bind_int64 = sqlite.sqlite3_bind_int64;
-pub const sqlite3_bind_double = sqlite.sqlite3_bind_double;
-pub const sqlite3_bind_text = sqlite.sqlite3_bind_text;
-pub const sqlite3_bind_blob = sqlite.sqlite3_bind_blob;
-pub const sqlite3_column_type = sqlite.sqlite3_column_type;
-pub const sqlite3_column_int = sqlite.sqlite3_column_int;
-pub const sqlite3_column_int64 = sqlite.sqlite3_column_int64;
-pub const sqlite3_column_double = sqlite.sqlite3_column_double;
-pub const sqlite3_column_text = sqlite.sqlite3_column_text;
-pub const sqlite3_column_bytes = sqlite.sqlite3_column_bytes;
-pub const sqlite3_column_blob = sqlite.sqlite3_column_blob;
-pub const sqlite3_last_insert_rowid = sqlite.sqlite3_last_insert_rowid;
-pub const sqlite3_changes = sqlite.sqlite3_changes;
 
 pub const db = @import("db.zig");
 pub const stmt = @import("stmt.zig");
