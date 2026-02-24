96ad40a1a2f7ec25ebd156e0529128cd00992298
MysticalDevil <jinpeng7981@outlook.com>
2026-02-24 20:05:56 +0800

    refactor: normalize integration test filenames

--- tests/integration/all.zig
+++ tests/integration/all.zig
@@ -4,14 +4,14 @@ comptime {
     _ = @import("text.zig");
     _ = @import("prepare_step.zig");
     _ = @import("stmt_prepare_step.zig");
-    _ = @import("stmt_bindall.zig");
+    _ = @import("stmt_bind_all.zig");
     _ = @import("insert_update.zig");
     _ = @import("stmt_column_owned.zig");
-    _ = @import("findbyid.zig");
-    _ = @import("findone.zig");
-    _ = @import("float_time.zig");
-    _ = @import("findmany.zig");
+    _ = @import("find_by_id.zig");
+    _ = @import("find_one.zig");
+    _ = @import("float_epoch_millis.zig");
+    _ = @import("find_many.zig");
     _ = @import("expected_errors.zig");
     _ = @import("errmsg.zig");
-    _ = @import("owned.zig");
+    _ = @import("owned_row.zig");
 }
