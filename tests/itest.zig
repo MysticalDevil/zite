68f2c5182135823425708d28171fb81ad3e439a2
MysticalDevil <jinpeng7981@outlook.com>
2026-02-24 18:33:59 +0800

    refactor: drop integration test filename prefix

--- tests/itest.zig
+++ tests/itest.zig
@@ -5,17 +5,17 @@ pub const std_options: std.Options = .{
 };
 
 comptime {
-    _ = @import("integration/integration_create_table.zig");
-    _ = @import("integration/integration_prepare_step.zig");
-    _ = @import("integration/integration_stmt_prepare_step.zig");
-    _ = @import("integration/integration_stmt_bindall.zig");
-    _ = @import("integration/integration_insert_update.zig");
-    _ = @import("integration/integration_stmt_column_owned.zig");
-    _ = @import("integration/integration_getbyid.zig");
-    _ = @import("integration/integration_findone.zig");
-    _ = @import("integration/integration_float_time.zig");
-    _ = @import("integration/integration_findmany.zig");
-    _ = @import("integration/integration_expected_errors.zig");
-    _ = @import("integration/integration_errmsg.zig");
-    _ = @import("integration/integration_owned.zig");
+    _ = @import("integration/create_table.zig");
+    _ = @import("integration/prepare_step.zig");
+    _ = @import("integration/stmt_prepare_step.zig");
+    _ = @import("integration/stmt_bindall.zig");
+    _ = @import("integration/insert_update.zig");
+    _ = @import("integration/stmt_column_owned.zig");
+    _ = @import("integration/getbyid.zig");
+    _ = @import("integration/findone.zig");
+    _ = @import("integration/float_time.zig");
+    _ = @import("integration/findmany.zig");
+    _ = @import("integration/expected_errors.zig");
+    _ = @import("integration/errmsg.zig");
+    _ = @import("integration/owned.zig");
 }
